f60db47fd583e822e5774106f49de9f9e935fc7d
Juan Jose <garciajuanjose224@gmail.com>
2023-06-24 00:32:00 -0500

    Tarjetas de duplicado y eliminacion

---
+++ src/App.vue
@@ -0,0 +1,55 @@
+<script setup>
+import { ref } from 'vue';
+import Cardcomponent from './components/Cardcomponent.vue';
+
+const users= ref([
+  {
+    name: "La Chilindrina",
+    correo: "lachilli23@gmail.com"
+  },
+  {
+    name: "La Popis",
+    correo: "lapopi23@gmail.com"
+  }
+])
+
+const borrarTarjeta= function(index){
+  users.value.splice(index, 1)
+}
+
+let name,correo;
+
+const addusers=function(){
+ const newUser = {
+  name: name,
+  correo: correo
+ }
+
+ users.value.unshift(newUser)
+}
+
+const duplicarTarjeta= function(name,correo){
+  const newUser = {
+  name: name,
+  correo: correo
+ }
+
+ users.value.unshift(newUser)
+}
+
+</script>
+
+<template>
+  <Cardcomponent v-for="(user,index) in users" :key="index" :name="user.name" :correo="user.correo" :index="index" @borrarTarjeta="borrarTarjeta" @duplicarTarjeta="duplicarTarjeta" />
+  <div class="mt-3">
+    <label>Ingrese su nombre</label>
+    <br>
+    <input type="text" placeholder="Nombre" v-model="name">
+    <br>
+    <label>Ingrese su correo</label>
+    <br>
+    <input type="text" placeholder="Correo" v-model="correo">
+    <br>
+    <button type="submit" class="mt-2 bg-orange-500 w-20 rounded-lg hover:bg-orange-400 hover:text-white" @click="addusers">Enviar</button>
+  </div>
+</template>
---
+++ src/components/Cardcomponent.vue
@@ -0,0 +1,48 @@
+<script setup>
+const mensajealerta= function(){
+    alert("Buenas careburros")
+}
+
+const props = defineProps(
+    {
+        name: {
+            type: String,
+            default: "No tenemos nombre AY NO 😉"
+        },
+        correo: {
+            type: String,
+            default: "No tenemos correo AY NO 😂"
+        },
+        index:{
+            type: Number
+        }
+    }
+)
+
+const borrarTarjeta= function(index){
+    emit("borrarTarjeta",index)
+}
+
+const duplicarTarjeta= function(name,correo){
+    emit("duplicarTarjeta",name,correo)
+}
+
+const emit = defineEmits(["borrarTarjeta","duplicarTarjeta"])
+</script>
+
+
+<template>
+    <div class="bg-red-800 w-[250px] h-[200px] rounded-lg">
+        <div class="flex justify-center items-center mt-24">
+            <img src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg" class="w-[200px] mt-4 rounded-lg" @click="mensajealerta">
+            <span class="bg-amber-500 w-6 rounded-md ml-1 mb-24 flex justify-center items-center cursor-pointer" @click="borrarTarjeta(props.index)">X</span>
+            <span class="bg-amber-500 w-6 rounded-md ml-1 mb-24 flex justify-center items-center cursor-pointer" @click="duplicarTarjeta(props.name,props.correo)">ADD </span>
+        </div>
+        <div class="flex justify-center items-center">
+            <div>
+                <p>{{ props.name }}</p>
+                <p>{{ props.correo }}</p>
+            </div>
+        </div>
+    </div>
+</template>
